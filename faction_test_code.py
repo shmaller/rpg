fbc17d530d3e8d4dcb44be2b277a7098d1ed363d
Nicholas Boni <nick@nicholasboni.com>
2023-11-21 03:06:25 -0500

    fix faction opinion check from names to class instances

--- faction_test_code.py
+++ faction_test_code.py
@@ -9,27 +9,31 @@ def main():
     Monks = Faction('Monks')
 
     archers_opinions = {
-        'Wizards': -0.7,
-        'Warriors': 0.6,
-        'Monks': 0.1
+        Archers: 0.6,
+        Wizards: -0.7,
+        Warriors: 0.6,
+        Monks: 0.1
     }
 
     wizards_opinons = {
-        'Archers': -0.5,
-        'Warriors': -0.1,
-        'Monks': 0.7
+        Wizards: 0.3,
+        Archers: -0.5,
+        Warriors: -0.1,
+        Monks: 0.7
     }
 
     warriors_opinions = {
-        'Archers': -0.9,
-        'Wizards': -0.2,
-        'Monks': 0.8
+        Warriors: 0.9,
+        Archers: -0.9,
+        Wizards: -0.2,
+        Monks: 0.8
     }
 
     monks_opinions = {
-        'Archers': -0.3,
-        'Warriors': 0.2,
-        'Wizards': 0.5
+        Monks: 0.8,
+        Archers: -0.3,
+        Warriors: 0.2,
+        Wizards: 0.5
     }
 
     Archers.opinions = archers_opinions
@@ -58,20 +62,64 @@ Choose carefully, for enmities abound: ").lower()
         input('idk what happened.')
         return
 
-    allegiance = float(input("How strongly do you feel connected\
+    allegiance = float(input("How strongly do you feel connected \
 to that path? 0-1: "))
     
     hero.allegiance = allegiance
 
-    Heiffa = Archer(hero,name='Heiffa')
-    Gorgo = Wizard(hero,name='Gorgo')
-    Bof = Warrior(hero,name='Bof')
-    Malak = Monk(hero,name='Malak')
+    Heiffa = Archer(name='Heiffa')
+    Gorgo = Wizard(name='Gorgo')
+    Bof = Warrior(name='Bof')
+    Malak = Monk(name='Malak')
 
-    hero.chat(Heiffa)
-    hero.chat(Gorgo)
-    hero.chat(Bof)
-    hero.chat(Malak)
+    Heiffa.faction = Archers
+    Gorgo.faction = Wizards
+    Bof.faction = Warriors
+    Malak.faction = Monks
+
+    Heiffa.allegiance = 1
+    Heiffa.CHM = 1
+
+    Gorgo.allegiance = 0.2
+    Gorgo.CHM = 20
+
+    Bof.allegiance = 1
+    Bof.CHM = 8
+
+    Malak.allegiance = 0.7
+    Malak.CHM = 100
+
+    hero.CHM = 100
+
+    print(f'''
+          Hero CHM: {hero.CHM}
+          Hero faction: {hero.faction.name}
+          
+          Heiffa CHM: {Heiffa.CHM}
+          Heiffa opinion: {Heiffa.determine_response(hero)}
+          Heiffa factional: {Heiffa.get_factional_opinion(hero)}
+          Archers factional: {Archers.opinion(hero)}
+
+          Gorgo CHM: {Gorgo.CHM}
+          Gorgo opinion: {Gorgo.determine_response(hero)}
+          Gorgo factional: {Gorgo.get_factional_opinion(hero)}
+          Wizards factional: {Wizards.opinion(hero)}
+
+          Bof CHM: {Bof.CHM}
+          Bof opinion: {Bof.determine_response(hero)}
+          Bof factional: {Bof.get_factional_opinion(hero)}
+          Warriors factional: {Warriors.opinion(hero)}
+          
+          Malak CHM: {Malak.CHM}
+          Malak opinion: {Malak.determine_response(hero)}
+          Malak factional: {Malak.get_factional_opinion(hero)}
+          Monks factional: {Monks.opinion(hero)}
+          ''')
+
+    Heiffa.chat(hero)
+    Gorgo.chat(hero)
+    Bof.chat(hero)
+    Malak.chat(hero)
 
 if __name__ == '__main__':
     main()
